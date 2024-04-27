a32afb15219092b83a6214efc02abb911079e41e
SE92036 <henrique.souza1@external.stellantis.com>
2024-04-27 14:30:10 +0100

    NgRx introduction

---
+++ NgRx/01-starting-setup-standalone/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+import { CounterOutputComponent } from './counter-output/counter-output.component';
+import { CounterControlsComponent } from './counter-controls/counter-controls.component';
+import {StoreModule} from "@ngrx/store";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [
+    CounterOutputComponent,
+    CounterControlsComponent,
+  ],
+})
+export class AppComponent {}
---
+++ NgRx/08-practice-second-action-standalone/src/main.ts
@@ -0,0 +1,10 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideStore } from '@ngrx/store';
+
+import { AppComponent } from './app/app.component';
+import { counterReducer } from './app/store/counter.reducer';
+import {CounterEffects} from "./app/store/counter.effects";
+
+bootstrapApplication(AppComponent, {
+  providers: [provideStore({ counter: counterReducer }), provideEffects([CounterEffects])],
+});
